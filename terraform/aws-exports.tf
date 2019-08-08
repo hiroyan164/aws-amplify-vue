6eb95e39234b7f56647306f99b8a4f4722266682
sabmeua <sabme.ua@gmail.com>
2019-08-08 15:02:29 +0900

    Add terraform configuration equivalent for "amplify add auth"

--- terraform/aws-exports.tf
+++ terraform/aws-exports.tf
@@ -3,6 +3,11 @@ resource "local_file" "aws_exports_js" {
   content  = <<EOF
 const awsmobile = {
     "aws_project_region": "${var.aws_default_region}",
+    "aws_cognito_identity_pool_id": "${aws_cognito_identity_pool.idpool.id}",
+    "aws_cognito_region": "${var.aws_default_region}",
+    "aws_user_pools_id": "${aws_cognito_user_pool.userpool.id}",
+    "aws_user_pools_web_client_id": "${aws_cognito_user_pool_client.webclient.id}",
+    "oauth": {},
 };
 export default awsmobile;
 EOF
