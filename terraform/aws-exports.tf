0c2c231c6726ca2f42745ecedfca3ad9807dc916
sabmeua <sabme.ua@gmail.com>
2019-08-09 15:02:37 +0900

    Add terraform configuration equivalent for "amplify add api"

--- terraform/aws-exports.tf
+++ terraform/aws-exports.tf
@@ -10,6 +10,9 @@ const awsmobile = {
     "oauth": {},
     "aws_user_files_s3_bucket": "${aws_s3_bucket.storage_bucket.bucket}",
     "aws_user_files_s3_bucket_region": "${var.aws_default_region}",
+    "aws_appsync_graphqlEndpoint": "${aws_appsync_graphql_api.todos_api.uris.GRAPHQL}",
+    "aws_appsync_region": "${var.aws_default_region}",
+    "aws_appsync_authenticationType": "${aws_appsync_graphql_api.todos_api.authentication_type}"
 };
 export default awsmobile;
 EOF
